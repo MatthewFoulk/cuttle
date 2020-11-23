43336eb4b64b7b137f70e25524a91e2d5d4bb901
Matthew Foulk <mewuzhere12@gmail.com>
2020-11-23 14:09:26 -0500

    Added card initialization and str

---
+++ CardGame.py
@@ -0,0 +1,55 @@
+"""
+Module for implimenting a card game
+"""
+
+# Holds 
+SUITS = {0: "Spade", 1: "Heart", 2: "Diamond", 3: "Club"}
+VALUES = {1: "Ace", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7",
+            8: "8", 9: "9", 10: "10", 11: "Jack", 12: "Queen", 13: "King"}
+
+
+class Card():
+    """
+    Class for implimenting a playing card from a standard deck.
+    Each card has both a suit and value.
+    """
+
+    def __init__(self, value, suit):
+        """
+        Initialize playing card.
+        Each playing card has:
+            - 'value': an integer value between 1 to 13: 1 for Ace,
+                    2 through 10 for numerical cards, 11 for Jack,
+                    12 for Queen, 13 for King
+            - 'suit': an integer value between 0 to 3: 0 for Spade,
+                    1 for Heart, 2 for Diamond, 3 for Club
+        """
+        if value < 1 or value > 13:
+            raise ValueError(f"""Illegal card value attempted. Acceptable values
+                are between 1 to 13. You tried: {value}""")
+
+        if suit < 0 or suit > 3:
+            raise ValueError(f"""Illegal suit attempted. Acceptable values
+                are between 0 to 3. You tried: {suit}""")
+
+        self.value = value
+        self.suit = suit
+    
+    def __str__(self):
+        """
+        String representation of a card object
+        """
+        return f"{VALUES[self.value]} of {SUITS[self.suit]}"
+        
+
+
+class Deck():
+    """
+    Class for implimenting a standard playing card deck.
+    """
+
+    def __init__(self):
+        pass
+    
+    def shuffle(self):
+        pass
